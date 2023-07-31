49d50b9321340cb644413155b1349528cac1fc98
brianyjtai1994 <brianyjtai1994@gmail.com>
2023-07-31 23:07:51 +0800

    add: `Vector.push()`

--- vector.zig
+++ vector.zig
@@ -2,6 +2,15 @@ const std = @import("std");
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
+fn betterMemSize(given: usize) usize {
+    var better: usize = given;
+    better +|= 8 + (better >> 1);
+    while (better < given) {
+        better +|= 8 + (better >> 1);
+    }
+    return better;
+}
+
 fn Vector(comptime T: type) type {
     return struct {
         const Self = @This();
@@ -30,6 +39,41 @@ fn Vector(comptime T: type) type {
                 .owner = owner,
             };
         }
+
+        inline fn setIndex(self: Self, ind: usize, val: T) void {
+            self.dat[ind] = val;
+        }
+
+        inline fn getIndex(self: Self, ind: usize) T {
+            return self.dat[ind];
+        }
+
+        fn push(self: *Self, val: T) Allocator.Error!void {
+            if (@sizeOf(T) == 0) {
+                self.size = std.math.maxInt(usize);
+            } else {
+                var new_size: usize = self.dat.len + 1;
+                if (self.size < new_size) {
+                    new_size = betterMemSize(self.size);
+                }
+
+                const old_mem = self.dat.ptr[0..self.size];
+                if (self.owner.resize(old_mem, new_size)) {
+                    self.size = new_size;
+                } else {
+                    const new_mem = try self.owner.alignedAlloc(T, @alignOf(T), new_size);
+                    @memcpy(new_mem[0..self.dat.len], self.dat);
+                    self.owner.free(old_mem);
+                    self.dat.ptr = new_mem.ptr;
+                    self.size = new_mem.len;
+                }
+            }
+
+            std.debug.assert(self.dat.len < self.size);
+
+            self.dat.len += 1;
+            self.dat[self.dat.len - 1] = val;
+        }
     };
 }
 
@@ -51,10 +95,24 @@ test "Vector.init" {
     }
     {
         var vec = try Vector(i32).init(arena.allocator(), 10);
-        vec.dat[3] = 4;
+        vec.setIndex(3, 4);
 
         try testing.expect(vec.dat.len == 10);
         try testing.expect(vec.size == 10);
-        try testing.expect(vec.dat[3] == 4);
+        try testing.expect(vec.getIndex(3) == 4);
+    }
+}
+
+test "Vector.push" {
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+
+    {
+        var vec = try Vector(i32).init(arena.allocator(), 0);
+        for (0..10) |i| {
+            try vec.push(@intCast(i));
+            try testing.expect(vec.getIndex(i) == i);
+        }
+        try testing.expect(vec.dat.len == 10);
     }
 }
